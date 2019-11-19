89158641ddfb1b17326d89837567a74db4eacad0
ayikfour <ayikfour4@gmail.com>
2019-11-19 23:03:56 +0700

    Remove Preprocessing Legacy

--- controller/informationRetrieval.js
+++ controller/informationRetrieval.js
@@ -1,63 +1,63 @@
 const IO = require("../IO");
 class Retrieval {
-   constructor() {}
+  constructor() {}
 
-   normalized(documents = []) {
-      let square = documents.map(document => {
-         return document.map(TFIDF => {
-            return TFIDF ** 2;
-         });
+  normalized(documents = []) {
+    let square = documents.map(document => {
+      return document.map(TFIDF => {
+        return TFIDF ** 2;
+      });
+    });
+
+    let length = square.map(document => {
+      return (
+        document.reduce((prev, current) => {
+          return prev + current;
+        }) ** 0.5
+      );
+    });
+
+    let normalize = documents.map((document, index) => {
+      return document.map(TFIDF => {
+        return TFIDF / length[index];
+      });
+    });
+
+    return normalize;
+  }
+
+  cossim(TFIDF = [], queries = []) {
+    queries = queries.reduce((prev, current) => {
+      return prev.concat(current);
+    });
+
+    let documentSimilarity = TFIDF.map(document => {
+      let dotProduct = document.map((token, index) => {
+        return token * queries[index];
       });
 
-      let length = square.map(document => {
-         return (
-            document.reduce((prev, current) => {
-               return prev + current;
-            }) ** 0.5
-         );
+      let sum = dotProduct.reduce((prev, current) => {
+        return prev + current;
       });
 
-      let normalize = documents.map((document, index) => {
-         return document.map(TFIDF => {
-            return TFIDF / length[index];
-         });
-      });
+      return sum;
+    });
 
-      return normalize;
-   }
+    return documentSimilarity;
+  }
 
-   cossim(TFIDF = [], queries = []) {
-      queries = queries.reduce((prev, current) => {
-         return prev.concat(current);
-      });
+  rankedRetrieval(cossim = []) {
+    cossim = cossim.map((similarity, index) => {
+      return { name: `document ${index + 1}`, similarity };
+    });
 
-      let documentSimilarity = TFIDF.map(document => {
-         let dotProduct = document.map((token, index) => {
-            return token * queries[index];
-         });
-
-         let sum = dotProduct.reduce((prev, current) => {
-            return prev + current;
-         });
-
-         return sum;
-      });
-
-      return document;
-   }
-
-   rankedRetrieval(cossim = []) {
-      cossim = cossim.map((similarity, index) => {
-         return { name: `document ${index + 1}`, similarity };
-      });
-
-      return cossim
-         .sort((a, b) => {
-            return a.similarity - b.similarity;
-         })
-         .reverse()
-         .filter(document => document.similarity > 0);
-   }
+    return cossim
+      .sort((a, b) => {
+        return a.similarity - b.similarity;
+      })
+      .reverse()
+      .filter(document => document.similarity > 0);
+  }
 }
 
 module.exports = Retrieval;
